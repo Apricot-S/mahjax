b8c19d0e12e5810420b1693594852938e620d35e
nissymori <kyoudai.630@gmail.com>
2026-01-10 17:45:23 +1100

    copy and init

--- mahjax/red_mahjong/yaku.py
+++ mahjax/red_mahjong/yaku.py
@@ -21,9 +21,9 @@ import jax.numpy as jnp
 import numpy as np
 
 from mahjax._src.types import Array
-from mahjax.no_red_mahjong.action import Action
-from mahjax.no_red_mahjong.hand import Hand
-from mahjax.no_red_mahjong.meld import EMPTY_MELD, Meld
+from mahjax.red_mahjong.action import Action
+from mahjax.red_mahjong.hand import Hand
+from mahjax.red_mahjong.meld import EMPTY_MELD, Meld
 
 DIR = os.path.join(os.path.dirname(__file__), "../_src/cache")
 
